2e6ca91bda6a86d6f0c65d1c9a58730c14e0b774
ACM <ACM@CHOUdeMacBook-Pro.local>
2018-01-13 03:08:08 +0800

    revised homework1

--- BasicAlgos/src/Assignments/HW1/PercolationStats.java
+++ BasicAlgos/src/Assignments/HW1/PercolationStats.java
@@ -2,33 +2,34 @@
  * PercolationStats.java
  * Todo: perform the Monte-Carlo experiments for finding the percolation threshold
  */
-package Assignments.HW1;
+
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-	private double[] perThresh;	// percolation threshold of all the experiments
+	private final double[] perThresh;	// percolation threshold of all the experiments
+	private static final double CONFIDENCE_95 = 1.96;	// hold the 95% confident intervals
 	
 	public PercolationStats(int n, int trials) {
 		// perform trials independent experiments on an n-by-n grid
-		if (n<=0 || trials <=0) throw new IllegalArgumentException();
+		if (n <= 0 || trials <= 0) throw new IllegalArgumentException();
 		
 		perThresh = new double[trials];
-		for (int i=0; i<trials; i++) {
+		for (int i = 0; i < trials; i++) {
 			Percolation per = new Percolation(n);
-			while(true) {
+			while (true) {
 				int select;
-				while(per.isOpen((int)(select = StdRandom.uniform(0, n*n))/n+1, select%n+1)) {
-					select = StdRandom.uniform(0, n*n);
+				while (per.isOpen((select = StdRandom.uniform(0, n * n)) / n + 1, select % n + 1)) {
+					select = StdRandom.uniform(0, n * n);
 				}
-				int row = select/n+1;
-				int col = select%n+1;
+				int row = select / n + 1;
+				int col = select % n + 1;
 				per.open(row, col);
-				if(per.percolates()) {
+				if (per.percolates()) {
 					break;
 				}
 			}
-				perThresh[i] = (double)(per.numberOfOpenSites())/(n*n);
+				perThresh[i] = (double)(per.numberOfOpenSites()) / (n * n);
 		}
 		
 	}
@@ -45,12 +46,12 @@ public class PercolationStats {
 	
 	public double confidenceLo() {
 		// low  endpoint of 95% confidence interval
-		return mean() - 1.96*stddev()/Math.sqrt(perThresh.length);
+		return mean() - CONFIDENCE_95 * stddev() / Math.sqrt(perThresh.length);
 	}
 	
 	public double confidenceHi() {
 		// high endpoint of 95% confidence interval
-		return mean() + 1.96*stddev()/Math.sqrt(perThresh.length);
+		return mean() + CONFIDENCE_95 * stddev() / Math.sqrt(perThresh.length);
 	}
 	
 	public static void main(String args[]) {
@@ -58,9 +59,9 @@ public class PercolationStats {
 		int n = Integer.parseInt(args[0]);
 		int trials = Integer.parseInt(args[1]);
 		PercolationStats ps = new PercolationStats(n, trials);
-		System.out.println("mean                    ="+ps.mean());
-		System.out.println("stddev                  ="+ps.stddev());
-		System.out.println("95% confidence interval = ["+ps.confidenceLo()+","+ps.confidenceHi()+"]");
+		System.out.println("mean                    =" + ps.mean());
+		System.out.println("stddev                  =" + ps.stddev());
+		System.out.println("95% confidence interval = [" + ps.confidenceLo() + "," + ps.confidenceHi() + "]");
 		
 	   }
 }
